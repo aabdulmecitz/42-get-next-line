d7aa754a3457652e21f406857f49f19df988aadf
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-08-16 13:40:03 -0400

    somethingsssdfgs

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 02:27:39 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/08/09 02:27:40 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/08/16 11:54:12 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdio.h>
+
+char    *get_next_line(int fd)
+{
+    char *str;
+    
+    str = (char *)malloc(4*sizeof(char));
+    while()
+    read(fd, str, 4);
+    return str;
+}
+int main(int argc, char const *argv[])
+{
+    int fd = open("deneme.txt", O_WRONLY || O_CREAT);
+    
+    printf("%s", get_next_line(fd));
+    return 0;
+}
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aabdulmecitz <aabdulmecitz@student.42.f    +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 02:27:46 by aabdulmecit       #+#    #+#             */
-/*   Updated: 2024/08/09 02:39:34 by aabdulmecit      ###   ########.fr       */
+/*   Updated: 2024/08/16 11:42:29 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #define GET_NEXT_LINE
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE
+#define BUFFER_SIZE = 4;
 #endif
 
 #include <stdlib.h>
